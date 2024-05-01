8701e900f1c76f0495fd7cbbc2fb77a07f73b01d
Belaptow <gasmirnov99@gmail.com>
2024-05-01 19:07:34 +0300

    Add project files.

---
+++ TestingSuite/DirectumProxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBroker;
+
+namespace TestingSuite
+{
+    public static class DirectumProxy
+    {
+        public static IBroker Resolve()
+        {
+            return null;//new BrokerService();
+        }
+        public static bool Wait(Guid queueId, Func<bool> work)
+        {
+
+
+            return false;
+        }
+    }
+}
---
+++ TestingSuite/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Tasks;
+using TaskBroker;
+using SharedLib;
+
+
+Console.WriteLine("Hello, World!");
+
+var logger = new ActionLogger(Console.WriteLine, Console.WriteLine);
+
+var limit = 10;
+var broker = new BrokerService(logger);
+
+var queueTasks = new List<Task>();
+var source = new CancellationTokenSource();
+
+var queues = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid() };
+queues.ForEachAsync(
+    queue =>
+        Enumerable.Range(0, limit).ForEachAsync(
+            test =>
+                broker.EnqueueAndWait(queue, () =>
+                {
+                    var timeout = new Random().Next(5, 15);
+                    logger.DebugFormat("doin work for {0} sec.", timeout);
+                    Thread.Sleep(timeout * 1000);
+                    return true;
+                }, TimeSpan.FromSeconds(1000))
+            )
+    );
+Console.WriteLine("all finished");
